d77e868055b4667185a585db5e61efeb52b61a98
Natthakrit Lalert <99980994+nlalert@users.noreply.github.com>
2024-11-16 10:05:26 +0700

    add Tetromino

---
+++ Assets/Scripts/Tetromino.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public enum Tetromino {
+    I,
+    O,
+    T,
+    J,
+    L,
+    S,
+    Z
+}
+
+[Serializable]
+public struct TetrominoData {
+    public Tetromino tetromino;
+    public Tile tile;
+    public Vector2Int[] cells { get; private set; }
+
+    public void Initialize(){
+        cells = Data.Cells[tetromino];
+    } 
+}
