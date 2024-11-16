4cbd2c0338b1b6d7ea5dedae3da5db26c94ec03a
Natthakrit Lalert <99980994+nlalert@users.noreply.github.com>
2024-11-16 23:19:45 +0700

    add spawning system

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,6 +1,38 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Board : MonoBehaviour
 {
+    public Tilemap tilemap { get; private set; }
+    public Piece activePiece { get; private set; }
     public TetrominoData[] tetrominos;
+    public Vector3Int spawnPosition;
+
+    private void Awake(){
+        tilemap = GetComponentInChildren<Tilemap>();
+        activePiece = GetComponentInChildren<Piece>();
+
+        for (int i = 0; i < tetrominos.Length; i++) {
+            tetrominos[i].Initialize();
+        }
+    }
+
+    private void Start(){
+        SpawnPiece();
+    }
+
+    private void SpawnPiece(){
+        int random = Random.Range(0, tetrominos.Length);
+        TetrominoData data = tetrominos[random];
+
+        activePiece.Initialize(this, spawnPosition, data);
+        Set(activePiece);
+    }
+
+    public void Set(Piece piece){
+        for (int i = 0; i < piece.cells.Length; i++){
+            Vector3Int tilePosition = piece.cells[i] + piece.position;
+            tilemap.SetTile(tilePosition, piece.data.tile);
+        }
+    }
 }
